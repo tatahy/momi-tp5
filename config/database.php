de44e5ee2c836c30a5000f7b4c9d9307a62920d6
tatahy <hy135@139.com>
2020-01-24 02:09:23 +0800

    update TheForm.vue

--- config/database.php
+++ config/database.php
@@ -67,7 +67,8 @@ return [
     	// 数据库类型
     	'type'            => 'mysql',
     	// 服务器地址
-    	'hostname'        => '127.0.0.1',
+    	//'hostname'        => '127.0.0.1',
+    	'hostname'        => 'localhost',
     	// 数据库名
     	'database'        => 'momi',
     	// 用户名
